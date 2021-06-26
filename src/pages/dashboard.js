63dc42556ca755664397851045a043b0051cd7cb
AlthafHussainK <althafakm@gmail.com>
2021-06-26 22:16:18 +0530

    create dashboard boilerplate

---
+++ src/pages/dashboard.js
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
+import Timeline from "../components/Timeline";
+
+export default function Dashboard() {
+  useEffect(() => {
+    document.title = 'Instagram'
+  }, [])
+
+  return (
+    <div className="bg-gray-background">
+      <Header />
+      <div className="grid">
+        <Timeline />
+        <Sidebar />
+      </div>
+    </div>
+  )
+}
