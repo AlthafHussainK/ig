7de03c36ac3644c566d079ff6b8f540311cdfeab
AlthafHussainK <althafakm@gmail.com>
2021-06-27 00:54:51 +0530

    pass props to user and suggestions

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -9,8 +9,8 @@ export default function Sidebar() {
 
   return (
     <div className="p-4">
-      <User />
-      <Suggestions />
+      <User username={username} fullName={fullName} />
+      <Suggestions userId={userId} />
     </div>
   )
 }
