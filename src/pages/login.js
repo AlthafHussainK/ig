35bb9b2f9c7293b15a13f67677cab91eb63652fc
AlthafHussainK <althafakm@gmail.com>
2021-06-26 21:55:06 +0530

    setup sign-up with firebase auth

--- src/pages/login.js
+++ src/pages/login.js
@@ -49,12 +49,14 @@ export default function Login() {
           placeholder="Email address"
           className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
           onChange={({ target }) => setEmailAddress(target.value)}
+          value={emailAddress}
         />
          <input aria-label="Enter your password" 
           type="password"
           placeholder="Password"
           className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
           onChange={({ target }) => setPassword(target.value)}
+          value={password}
         />
         <button 
           disabled={isInvalid}
@@ -69,7 +71,7 @@ export default function Login() {
       <div className="flex justify-center items-center flex-col w-full rounded bg-white p-4 border border-gray-primary">
         <p className="text-sm">
           Don't have an account?{` `}
-          <Link to="/signup" className="font-bold text-blue-medium">
+          <Link to={ROUTES.SIGN_UP} className="font-bold text-blue-medium">
             Signup
           </Link>
         </p>
--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from "react"
 import { Link, useHistory } from "react-router-dom"
 import FirebaseContext from "../context/firebase"
 import * as ROUTES from '../constants/routes'
+import { doesUsernameExist } from "../services/firebase"
 
 export default function SignUp() {
   const history = useHistory()
@@ -18,11 +19,36 @@ export default function SignUp() {
   const handleSignUp = async (event) => {
     event.preventDefault()
 
-    // try {
-      
-    // } catch (error) {
-      
-    // }
+    const usernameExists = await doesUsernameExist(username)
+    if (!usernameExists.length) {
+      try {
+        const  createdUserResult = await firebase
+          .auth()
+          .createUserWithEmailAndPassword(emailAddress, password)
+
+        await createdUserResult.user.updateProfile({
+          displayName: username
+        })
+
+        await firebase.firestore().collection('users').add({
+          userId: createdUserResult.user.uid,
+          username: username.toLocaleLowerCase(),
+          fullname,
+          emailAddress: emailAddress.toLocaleLowerCase(), 
+          following: [],
+          dateCreated: Date.now()
+        })
+
+        history.push(ROUTES.DASHBOARD)
+      } catch(error){
+        setFullname('')
+        setEmailAddress('')
+        setPassword('')
+        setError(error.message)
+      }
+    } else {
+      setError('Username is already taken, try another one.')
+    }
   }
 
   useEffect(() => {
@@ -48,24 +74,28 @@ export default function SignUp() {
           placeholder="Username"
           className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
           onChange={({ target }) => setUsername(target.value)}
+          value={username}
         />
         <input aria-label="Enter your full name" 
           type="text"
           placeholder="Full name"
           className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
           onChange={({ target }) => setFullname(target.value)}
+          value={fullname}
         />
         <input aria-label="Enter your email addres" 
           type="text"
           placeholder="Email address"
           className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
           onChange={({ target }) => setEmailAddress(target.value)}
+          value={emailAddress}
         />
          <input aria-label="Enter your password" 
           type="password"
           placeholder="Password"
           className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
           onChange={({ target }) => setPassword(target.value)}
+          value={password}
         />
         <button 
           disabled={isInvalid}
@@ -80,7 +110,7 @@ export default function SignUp() {
       <div className="flex justify-center items-center flex-col w-full rounded bg-white p-4 border border-gray-primary">
         <p className="text-sm">
           Have an account?{` `}
-          <Link to="/login" className="font-bold text-blue-medium">
+          <Link to={ROUTES.LOGIN} className="font-bold text-blue-medium">
             Login
           </Link>
         </p>
