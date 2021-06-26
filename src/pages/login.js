607c271c570cd29b837638b2d835d47137fa5c19
AlthafHussainK <althafakm@gmail.com>
2021-06-26 14:46:21 +0530

    finish login

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react"
 import { Link, useHistory } from "react-router-dom"
 import FirebaseContext from "../context/firebase"
+import * as ROUTES from '../constants/routes'
 
 export default function Login() {
   const history = useHistory()
@@ -12,7 +13,18 @@ export default function Login() {
   const [error, setError] = useState('')
   const isInvalid = password === '' || emailAddress === ''
 
-  const handleLogin = () => {}
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      await firebase.auth().signInWithEmailAndPassword(emailAddress, password)
+      history.push(ROUTES.DASHBOARD)
+    } catch (error) {
+      setEmailAddress('')
+      setPassword('')
+      setError(error.message)
+    }
+  }
 
   useEffect(() => {
     document.title = 'Login - Instagram'
