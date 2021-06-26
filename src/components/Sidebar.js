e2f6c672a871a43e460294489d0b50a218d664d8
AlthafHussainK <althafakm@gmail.com>
2021-06-27 00:38:58 +0530

    Sidebar initial

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,9 @@
-export default function Sidebar () {
+import useUser from "../hooks/use-user"
+
+export default function Sidebar() {
+  const x = useUser()
+  console.log('x', x)
+
   return (
     <p>Sidebar</p>
   )
