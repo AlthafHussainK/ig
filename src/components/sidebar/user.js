9e5d24005d1772532b7e7e53f3c0353aaf96b681
AlthafHussainK <althafakm@gmail.com>
2021-06-27 09:01:51 +0530

    sidebar user avatar

--- src/components/sidebar/user.js
+++ src/components/sidebar/user.js
@@ -1,3 +1,26 @@
-export default function User() {
-  return <p>User here</p>
+import { memo } from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+import Skeleton from 'react-loading-skeleton'
+
+const User = ({ username, fullName }) => 
+  !username && !fullName ? ( // OR is required
+    <Skeleton count={1} height={61} />
+  ) : (
+    <Link to={`/p/${username}`} className="grid grid-cols-4 gap-4 mb-6 items-center">
+      <div className="flex items-center justify-between col-span-1">
+        <img
+          className="rounded-full w-16 flex mr-3"
+          src={`/images/avatars/${username}.jpg`}
+          alt=""
+        />
+      </div>
+    </Link>
+ )
+
+export default User
+
+User.propTypes = {
+  username: PropTypes.string,
+  fullName: PropTypes.string
 }
