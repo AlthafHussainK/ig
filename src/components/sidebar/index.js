8b4889fb37f7d5e0098692a7a4bd19c40cdf1900
AlthafHussainK <althafakm@gmail.com>
2021-06-27 10:49:47 +0530

    fix bug: fileName to filename

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -4,12 +4,12 @@ import Suggestions from './suggestions'
 
 export default function Sidebar() {
   const {
-     user: { fullName, username, userId } 
+     user: { fullname, username, userId } 
     } = useUser()
-
+    
   return (
     <div className="p-4">
-      <User username={username} fullName={fullName} />
+      <User username={username} fullname={fullname} />
       <Suggestions userId={userId} />
     </div>
   )
--- src/components/sidebar/user.js
+++ src/components/sidebar/user.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import Skeleton from 'react-loading-skeleton'
 
-const User = ({ username, fullName }) => 
-  !username && !fullName ? ( // OR is required
+const User = ({ username, fullname }) => 
+  !username || !fullname ? ( 
     <Skeleton count={1} height={61} />
   ) : (
     <Link to={`/p/${username}`} className="grid grid-cols-4 gap-4 mb-6 items-center">
@@ -21,6 +21,6 @@ const User = ({ username, fullName }) =>
 export default User
 
 User.propTypes = {
-  username: PropTypes.string,
-  fullName: PropTypes.string
+  username: PropTypes.string.isRequired,
+  fullname: PropTypes.string.isRequired
 }
