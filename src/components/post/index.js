f6403dbe4f3bd6323c1a979736104bc697d39b9f
AlthafHussainK <althafakm@gmail.com>
2021-06-29 22:26:51 +0530

    update margin-bottom

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -6,7 +6,7 @@ import Image from './image'
 export default function Post({ content }) {
 
   return (
-    <div className="rounded col-span-4 border bg-white border-gray-primary mb-16">  
+    <div className="rounded col-span-4 border bg-white border-gray-primary mb-12">  
       <Header username={content.username} />
       <Image src={content.imageSrc} caption={content.caption} />
     </div>
