fa16ac3088a01959bd6ad5847a860a4ea2077857
AlthafHussainK <althafakm@gmail.com>
2021-06-30 00:24:52 +0530

    format date of post

--- src/components/post/comments.js
+++ src/components/post/comments.js
@@ -23,7 +23,7 @@ export default function Comments({ docId, comments: allComments, posted, comment
           </p>
         ))}
         <p className="text-gray-base uppercase text-xs mt-2">
-          {posted}
+          {formatDistance(posted, new Date())} ago
         </p>
       </div>
     </>
