37a969ef347c5e32e875d07bf58815d601306feb
AlthafHussainK <althafakm@gmail.com>
2021-06-30 00:17:25 +0530

    view more comments link

--- src/components/post/comments.js
+++ src/components/post/comments.js
@@ -4,9 +4,17 @@ import { formatDistance } from "date-fns";
 import { Link } from "react-router-dom";
 
 export default function Comments({ docId, comments: allComments, posted, commentInput }) {
+  const [comments, setComments] = useState(allComments)
+
   return (
     <>
-      <p>Comments go here</p>
+      <div className="p-4 pt-1 pb-4">
+        {comments.length >= 3 && (
+          <p className="text-sm text-gray-base mb-1 cursor-pointer">
+            View all {comments.length} comments
+          </p>
+        )}
+      </div>
     </>
   )
 }
