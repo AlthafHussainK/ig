23d795b0e1384ae91e18114d4f73d9fa9d3b7ecb
AlthafHussainK <althafakm@gmail.com>
2021-06-29 14:26:38 +0530

    suggested for you profiles

--- src/components/sidebar/suggested-profile.js
+++ src/components/sidebar/suggested-profile.js
@@ -1,3 +1,26 @@
+import { useState } from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+
 export default function SuggestedProfile({ userDocId, username, profileId, userId }) {
-  return <p>SuggestedProfiles {username}  </p>
+  const [followed, setFollowed] = useState(false)
+
+  return !followed ? (
+    <div className="flex flex-row items-center align-items justify-between">
+      <div className="flex items-center justify-between">
+        <img 
+          className="rounded-full w-8 flex mr-3"
+          src={`/images/avatars/${username}.jpg`}
+          alt=""
+        />
+      </div>
+    </div>
+  ) : null
+}
+
+SuggestedProfile.propTypes = {
+  userDocId: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  profileId: PropTypes.string.isRequired,
+  userId: PropTypes.string.isRequired
 }
