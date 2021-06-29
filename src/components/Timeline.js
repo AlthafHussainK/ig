4d7364c76d5c108267420eddc64c311779f33e76
AlthafHussainK <althafakm@gmail.com>
2021-06-29 18:31:53 +0530

    create custom hook usePhotos

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,4 +1,9 @@
+import Skeleton from "react-loading-skeleton"
+import usePhotos from "../hooks/use-photos"
+
 export default function Timeline () {
+  const { photos } = usePhotos()
+  
   return (
     <div className="container col-span-2">
       Timeline is here
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -33,8 +33,6 @@ export async function getSuggestedProfiles(userId, following) {
     .filter((profile) => profile.userId !== userId && !following.includes(profile.userId))
 }
 
-// updateLoggedInUserFollowing, updateFollowedUserFollowers
-
 export async function updateLoggedInUserFollowing(loggedInUserDocId, profileId, isFollowingProfile) {
 
   return firebase
@@ -59,4 +57,31 @@ export async function updateFollowedUserFollowers(profileDocId, loggedInUserDocI
         ? FieldValue.arrayRemove(loggedInUserDocId)
         : FieldValue.arrayUnion(loggedInUserDocId)
     })
+}
+
+export async function getPhotos(userId, following) {
+  const result = await firebase
+    .firestore()
+    .collection('photos')
+    .where('userId', 'in', following)
+    .get()
+
+  const userFollowedPhotos = result.docs.map((photo) => ({
+    ...photo.data(),
+    docId: photo.id
+  }))
+
+  const photosWithUserDetails = await Promise.all(
+    userFollowedPhotos.map(async (photo) => {
+      let userLikedPhoto = false
+      if (photo.likes.includes(userId)) {
+        userLikedPhoto = true
+      }
+      const user = await getUserByUserId(photo.userId)
+      const { username } = user[0]
+      return { username, ...photo, userLikedPhoto }
+    })
+  )
+
+  return photosWithUserDetails
 }
