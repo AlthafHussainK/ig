aef1de16f3a3691e10813cea9ee8d8136396eba0
AlthafHussainK <althafakm@gmail.com>
2021-06-30 01:16:22 +0530

    Add protected routes

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import * as ROUTES from './constants/routes';
 import UserContext from './context/user';
 import useAuthListener from './hooks/use-auth-listener';
 
+import ProtectedRoutes from './helpers/protected-route';
+
 const Login = lazy(() => import ('./pages/login'));
 const SignUp = lazy(() => import ('./pages/sign-up'));
 const Dashboard = lazy(() => import ('./pages/dashboard'));
 const NotFound = lazy(() => import ('./pages/not-found'));
-
 
 export default function App() {
   const { user } = useAuthListener()
@@ -20,7 +21,9 @@ export default function App() {
           <Switch>
             <Route path={ROUTES.LOGIN} component={Login} />
             <Route path={ROUTES.SIGN_UP} component={SignUp} />
-            <Route path={ROUTES.DASHBOARD} component={Dashboard} />
+            <ProtectedRoutes user={user} path={ROUTES.DASHBOARD} exact >
+              <Dashboard />
+            </ProtectedRoutes>
             <Route component={NotFound} />
           </Switch>
         </Suspense>
