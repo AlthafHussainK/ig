bcd5f1d6a7bc9e6a6c6ade7eb673b8ff8ff3d65c
AlthafHussainK <althafakm@gmail.com>
2021-06-29 21:36:22 +0530

    render images

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,12 +1,14 @@
 import { useRef } from "react"
 import PropTypes from 'prop-types'
 import Header from "./header"
+import Image from './image'
 
 export default function Post({ content }) {
 
   return (
     <div className="rounded col-span-4 border bg-white border-gray-primary mb-16">  
       <Header username={content.username} />
+      <Image src={content.imageSrc} caption={content.caption} />
     </div>
   )
 }
