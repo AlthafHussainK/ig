d45dcd56fb5f7746618b73fe4f9a7603ce20eca2
AlthafHussainK <althafakm@gmail.com>
2021-06-30 00:07:57 +0530

    comment section start

---
+++ src/components/post/comments.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import PropTypes from 'prop-types'
+import { formatDistance } from "date-fns";
+import { Link } from "react-router-dom";
+
+export default function Comments({ docId, comments: allComments, posted, commentInput }) {
+  return (
+    <>
+      <p>Comments go here</p>
+    </>
+  )
+}
