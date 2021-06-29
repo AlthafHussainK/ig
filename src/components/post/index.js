2f53a979a95295bfb4774705ca4a3db2a310856c
AlthafHussainK <althafakm@gmail.com>
2021-06-29 21:16:13 +0530

    add post proptypes

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -2,5 +2,19 @@ import { useRef } from "react"
 import PropTypes from 'prop-types'
 
 export default function Post({ content }) {
+  
+
   return <p>Posts are here</p>
+}
+
+Post.propTypes = {
+  content: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    imageSrc: PropTypes.string.isRequired,
+    caption: PropTypes.string.isRequired,
+    docId: PropTypes.bool.isRequired,
+    likes: PropTypes.array.isRequired,
+    comments: PropTypes.array.isRequired,
+    dateCreated: PropTypes.number.isRequired,
+  })
 }
