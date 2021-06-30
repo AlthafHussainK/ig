464643dfe4fc6c762a4e1f1648e3ed4f40a28044
AlthafHussainK <althafakm@gmail.com>
2021-06-30 22:25:40 +0530

    add follow button to profile

--- src/components/profile/header.js
+++ src/components/profile/header.js
@@ -40,6 +40,15 @@ export default function Header({
       <div className="flex items-center justify-center flex-col col-span-2">
         <div className="container flex items-center">
           <p className="text-2xl mr-4">{profileUsername}</p>
+          {activeBtnFollow && (
+            <button 
+              className="bg-blue-medium font-bold text-sm rounded text-white w-20 h-8"
+              type="button"
+              // onClick={handleToggleFollow}
+            >
+              {isFollowingProfile ? 'Unfollow' : 'Follow' }
+            </button>
+          )}
         </div>
       </div>
     </div>
