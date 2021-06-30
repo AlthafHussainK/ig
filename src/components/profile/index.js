04c19d0ecc3fdd9e74cba41198ba9a2858e5fca0
AlthafHussainK <althafakm@gmail.com>
2021-06-30 12:51:11 +0530

    user photos

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,31 +1,30 @@
 import { useReducer, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import Header from './header'
-import { getUserByUsername, getUserPhotosByUsername } from '../../services/firebase'
+import { getUserPhotosByUsername } from '../../services/firebase'
+import Photos from './photos'
 
 export default function Profile({ user }) {
   const reducer = (state, newState) => ({ ...state, ...newState })
   const initialState = { 
     profile: {},
-    photoCollection: [],
+    photosCollection: [],
     followerCount: 0
   }
 
-  const [{ profile, photoCollection, followerCount }, dispatch] = useReducer(reducer, initialState)
+  const [{ profile, photosCollection, followerCount }, dispatch] = useReducer(reducer, initialState)
 
   useEffect(() => {
     async function getProfileInfoAndPhotos() {
       const photos = await getUserPhotosByUsername(user.username)
-      console.log('photosssssss: ', photos)
-      // dispatch({ profile: user, photoCollection: photos, followerCount: user.followers.length })
+      dispatch({ profile: user, photosCollection: photos, followerCount: user.followers.length })
     }
-    if (user.username) {
-      getProfileInfoAndPhotos()
-    }
+    getProfileInfoAndPhotos()
   }, [user.username])
 
   return <>
    <Header />
+   <Photos photos={photosCollection} />
    <p>{user.username}' Profile</p>
   </>
 }
