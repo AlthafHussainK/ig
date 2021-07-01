c1d732f86cbb0be3425192c7d28c04e3b9dd7c73
AlthafHussainK <althafakm@gmail.com>
2021-07-01 12:06:49 +0530

    fetch photos

--- src/components/profile/photos.js
+++ src/components/profile/photos.js
@@ -3,7 +3,20 @@ import Skeleton from 'react-loading-skeleton'
 
 
 export default function Photos({ photos }) {
-  return null
+  console.log('----', photos)
+  return (
+    <div className="h-16 border-t border-gray-primary mt-12 pt-4">
+      <div className="grid grid-cols-3 gap-8 mt-4 mb-12">
+        {!photos ? (
+          <>
+            <Skeleton count={12} width={320} height={400} />
+          </>
+        ) : photos.length > 0 ? (
+          photos.map((photo) => <div key={photo.docId} className="relative group" />)
+        ) : null}
+      </div>
+    </div>
+  )
 }
 
 Photos.propTypes = {
